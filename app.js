00e139e8040f1a1b3b10c8a4a668c091fab8de43
Sunnatillo Azimjonov <monomusix001@gmail.com>
2024-03-14 23:43:56 +0300

    arr homework

---
+++ app.js
@@ -0,0 +1,31 @@
+// function namuna(arr) {
+//   let newarr = [];
+//   arr.forEach((el) => {
+//     console.log(el.indexOf());
+//   });
+//   newarr.push(el);
+// }
+// namuna([1, 2, 5, 6]);
+
+// 1 masala
+// function isEmpty() {
+//   let object = {
+//     john: 100,
+//     ann: 300,
+//     petr: 200,
+//   };
+//   for (i in object) {
+//     console.log(object[i] * 2);
+//   }
+// }
+// isEmpty();
+
+// 4
+// function menu() {
+//   let newarr;
+//   let arr = [1, 2, 3, 4, 5];
+//   arr.forEach((el) => {
+//     console.log(el);
+//   });
+// }
+// menu();
